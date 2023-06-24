d553f4b9fb48a598ead5cff055f8a4f57405d7a7
Evans A. Stepanov <evans@stepanov.email>
2023-06-23 20:33:27 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# My personal domain
+
+A very simple personal landing page, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+
+## Developing
+
+Once you've cloned this projecte installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of this app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex justify-center items-center p-5 flex-col h-screen space-y-5">
+	<h1 class="block text-2xl">Evans Stepanov</h1>
+	<p class="text-sm italic">Software Engineer</p>
+	<ul class="flex flex-row space-x-6 text-base">
+		<li>
+			<a class="text-green-600 hover:text-green-900 transition-all ease-in-out underline hover:no-underline" href="https://github.com/estepanov">GitHub</a>
+		</li>
+		<li>
+			<a class="text-green-600 hover:text-green-900 transition-all ease-in-out underline hover:no-underline" href="https://linkedin.com/in/evansstepanov/">LinkedIn</a>
+		</li>
+		<li>
+			<a class="text-green-600 hover:text-green-900 transition-all ease-in-out underline hover:no-underline" href="https://builtbyevans.com/">Projects</a>
+		</li>
+	</ul>
+</div>
